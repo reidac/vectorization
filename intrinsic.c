816dd9f3fadb32c3e806ae56a20866cabee986af
Andrew Reid <andrew.ce.reid@gmail.com>
2019-07-28 00:04:30 -0400

    Intrinsic working. Unclear if actually vectorized.

--- intrinsic.c
+++ intrinsic.c
@@ -5,10 +5,7 @@
 
 #define LIST_SIZE 16777216
 
-typedef union {
-  __m128i v;
-  int v1,v2,v3,v4;
-} elem;
+typedef __m128i elem;
   
 int main() {
   elem *a = (elem *)aligned_alloc(128,sizeof(elem)*LIST_SIZE);
@@ -16,22 +13,34 @@ int main() {
   elem *c = (elem *)aligned_alloc(128,sizeof(elem)*LIST_SIZE);
 
   for(int i=0;i<LIST_SIZE;++i) {
-    a[i].v1 = i*4; a[i].v2 = i*4+1; a[i].v3 = i*4+2; a[i].v4 = i*4+3;
-    b[i].v1 = (LIST_SIZE-i)*4; b[i].v2 = (LIST_SIZE-i)*4-1;
-    b[i].v3 = (LIST_SIZE-i)*4-2; b[i].v4 = (LIST_SIZE-i)*4-3;
+    a[i] = _mm_set_epi32(i*4,i*4+1,i*4+2,i*4+3);
+    b[i] = _mm_set_epi32((LIST_SIZE-i)*4,(LIST_SIZE-i)*4-1,
+			 (LIST_SIZE-i)*4-2,(LIST_SIZE-i)*4-3);
   }
 
-  // Vector loop, maybe...
+  // Vector loop, maybe.  Should OpenMP-parallelize.
   for(int i=0;i<LIST_SIZE;i++) {
-    c[i].v = a[i].v + b[i].v;
+    c[i] = a[i] + b[i];
   }
 
   for(int i=0;i<LIST_SIZE;i++) {
     if ((i<3) || (i>(LIST_SIZE-4))) {
-      printf("%d + %d = %d\n", a[i].v1,b[i].v1,c[i].v1);
-      printf("%d + %d = %d\n", a[i].v2,b[i].v2,c[i].v2);
-      printf("%d + %d = %d\n", a[i].v3,b[i].v3,c[i].v3);
-      printf("%d + %d = %d\n", a[i].v4,b[i].v4,c[i].v4);
+	printf("%d + %d = %d\n",
+	       _mm_extract_epi32(a[i],3),
+	       _mm_extract_epi32(b[i],3),
+	       _mm_extract_epi32(c[i],3));
+	printf("%d + %d = %d\n",
+	       _mm_extract_epi32(a[i],2),
+	       _mm_extract_epi32(b[i],2),
+	       _mm_extract_epi32(c[i],2));
+	printf("%d + %d = %d\n",
+	       _mm_extract_epi32(a[i],1),
+	       _mm_extract_epi32(b[i],1),
+	       _mm_extract_epi32(c[i],1));
+	printf("%d + %d = %d\n",
+	       _mm_extract_epi32(a[i],0),
+	       _mm_extract_epi32(b[i],0),
+	       _mm_extract_epi32(c[i],0));
     }
   }
   return 0;
